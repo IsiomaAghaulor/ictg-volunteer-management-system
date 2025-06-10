28ae53c034b4571bf2cdba227b4668dd0209c524
Isioma Aghaulor <91533503+IsiomaAghaulor@users.noreply.github.com>
2025-06-10 08:16:49 +0100

    feat/project set up

---
+++ src/main/java/com/lfc/volunteermgtsystem/VolunteerMgtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lfc.volunteermgtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerMgtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerMgtSystemApplication.class, args);
+    }
+
+}
